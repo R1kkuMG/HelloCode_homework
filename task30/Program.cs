168ce3522dcfcf53164f48e9429aa6ead04e5956
Maggie <alis-weasley@mail.ru>
2021-12-22 19:20:51 +0300

    Add task30

---
+++ task30/Program.cs
@@ -0,0 +1,19 @@
+﻿// 30. Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine($"{i} x {i} x {i} = {Math.Pow(i, 3)}");
+    }
+    else
+    {
+        Console.WriteLine($"{i} Нечетное");
+    }
+}
+
+
