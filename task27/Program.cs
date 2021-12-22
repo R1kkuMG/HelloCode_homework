c38555205a3c66b57b1c4dd05f962e60dc5f45a9
Maggie <alis-weasley@mail.ru>
2021-12-22 18:03:11 +0300

    Add task27

---
+++ task27/Program.cs
@@ -0,0 +1,18 @@
+﻿// 27. Определить количество цифр в числе
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int a = int.Parse(Console.ReadLine());
+
+int count = 0;
+int b = 0;
+ 
+while (a != 0)
+{
+    b = a % 10;
+    a = a / 10;
+    count++;
+}
+
+Console.WriteLine("Количество цифр: " + count); //Количество цифр: 0
+Console.ReadLine();
