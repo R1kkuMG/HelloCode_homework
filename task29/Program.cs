4b780e02bf711f09c4953b8c681e1474e245efee
Maggie <alis-weasley@mail.ru>
2021-12-22 18:47:30 +0300

    Add task29

---
+++ task29/Program.cs
@@ -0,0 +1,14 @@
+﻿// 29. Написать программу вычисления произведения чисел от 1 до N
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int n = int.Parse(Console.ReadLine());
+
+int sum = 1;
+
+for (int i = 1; i <= n; i++)
+{
+    sum = sum * i;
+}
+Console.WriteLine(sum);
+Console.ReadLine();
