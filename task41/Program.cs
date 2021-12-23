f6112090699c0d667adfb2349a43af1670c2bab9
Maggie <alis-weasley@mail.ru>
2021-12-23 20:58:33 +0300

    Add task41

---
+++ task41/Program.cs
@@ -0,0 +1,19 @@
+﻿// 41. Выяснить являются ли три числа сторонами треугольника
+
+Console.Clear();
+
+Console.Write("Введите строну а: ");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Введите строну b: ");
+double b = double.Parse(Console.ReadLine());
+Console.Write("Введите строну c: ");
+double c = double.Parse(Console.ReadLine());
+
+if ((a + b > c) && (b + c > a) && (a + c > b))
+{
+    Console.WriteLine("Это треугольник");
+}
+else
+{
+    Console.WriteLine("Это не треугольник");
+}
