de428a2131c7c3d723ec6b2892e5115ee7e33d82
Maggie <alis-weasley@mail.ru>
2021-12-26 21:48:53 +0300

    Add task49, NEED FIXING

---
+++ task49/Program.cs
@@ -0,0 +1,31 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+
+Console.Clear();
+
+void PrintArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j] / 2d}"); // TO DO
+        }
+    }
+}
+
+void FillArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1,10);
+        }
+    }
+}
+
+double[,] matrix = new double [4,3];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
