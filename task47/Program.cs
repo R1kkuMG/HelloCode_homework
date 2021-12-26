018c59a6efff2633687513e24f28abe14cc207e5
Maggie <alis-weasley@mail.ru>
2021-12-26 20:38:19 +0300

    Add task47

--- task47/Program.cs
+++ task47/Program.cs
@@ -1,8 +1,25 @@
 ﻿// 47. Написать программу копирования массива
 Console.Clear();
 
-int[] a = {1,2,3,4,5};
+void CopyArray()
+{
+    int[] a = {1,2,3,4,5};
+    for (int i = 0; i < a.Length; i++)
+    {
+        Console.Write(" " + a[i] + " ");
+    }
+    Console.WriteLine(" ");
 
-int [] b= new int[a.Length]; //New Array and the size of a which is 4
-
-Array.Copy(a,b,a.Length);
+    int[] b = new int[5]; //New Array and the size of a which is 4
+    for (int j = 0; j < b.Length; j++)
+    {
+        Array.Copy(a, 0, b, 0, 5);
+        // a - исходный массив
+        // 0 = начальный индекс в исходном массиве
+        // b = целевой массив
+        // 0 = начальный индекс в целевом массиве
+        // 5 = элементы для копирования
+        Console.Write(" " + b[j] + " ");
+    }
+}
+CopyArray();
