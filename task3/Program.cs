7e17e197449d834df6671ee806197a73e801ce72
Maggie <alis-weasley@mail.ru>
2021-12-29 20:17:28 +0300

    Update: По заданному номеру дня недели вывести его название

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,4 +1,8 @@
-﻿Console.Write("Введите число от 1 до 7: ");
+﻿ // 3. По заданному номеру дня недели вывести его название
+
+Console.Clear();
+
+Console.Write("Введите число от 1 до 7: ");
 string number = Console.ReadLine();
 
 if(number == "1")
