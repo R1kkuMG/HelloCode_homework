bbd799313a2e8dddbe742d79a3d795479b0e1332
Maggie <alis-weasley@mail.ru>
2021-12-29 19:50:40 +0300

    Update: Вывести квадрат числа

--- task0/Program.cs
+++ task0/Program.cs
@@ -1,7 +1,13 @@
 ﻿// 0. Вывести квадрат числа
-int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
-Console.WriteLine(numberA);
-int numberB = numberA;
-Console.WriteLine(numberB);
-int result = numberA * numberB;
-Console.WriteLine(result);
+Console.Clear();
+
+void Sqr()
+{
+    int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+    Console.WriteLine(numberA);
+    int numberB = numberA;
+    Console.WriteLine(numberB);
+    int result = numberA * numberB;
+    Console.WriteLine(result);
+}
+Sqr();
