e5e5fbf6242bb464f5a67635076b849cd7be1d2e
Maggie <alis-weasley@mail.ru>
2021-12-29 20:20:43 +0300

    Update: Написать программу вычисления значения функции y = f(a)

--- task5/Program.cs
+++ task5/Program.cs
@@ -1,4 +1,8 @@
-﻿int x = new Random().Next(1, 10); // 1 2 3 4 ... 9
+﻿// 5. Написать программу вычисления значения функции y = f(a)
+
+Console.Clear();
+
+int x = new Random().Next(1, 10); // 1 2 3 4 ... 9
 Console.WriteLine(x);
 int y = new Random().Next(1, 10); // 1 2 3 4 ... 9
 Console.WriteLine(y);
