d0105f2300197a7fe42bee726d6f88b1b47b2d47
Margarita <alis-weasley@mail.ru>
2022-01-26 22:56:49 +0300

    В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты

---
+++ task52/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// 52. В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
+
+Console.Clear();
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(1, 10);
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void IndexOf(int[,] col)
+{
+    for (int i = 0; i < col.GetLength(0); i++)
+    {
+        for (int j = 0; j < col.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 ==0)
+            {
+                col[i,j] = col[i,j] * col[i,j];
+                Console.Write($"{col[i,j]} ");
+            }
+        }
+    }
+}
+
+int[,] array = new int[4,4];
+FillArray(array);
+Console.WriteLine();
+IndexOf(array);
