66b16d0ff1ea5962cc81248bb62f4c93b4a5823a
Margarita <alis-weasley@mail.ru>
2022-01-26 23:20:56 +0300

    Update: В двумерном массиве n×k заменить четные элементы на противоположные

--- task50/Program.cs
+++ task50/Program.cs
@@ -2,49 +2,46 @@
 
 Console.Clear();
 
-void PrintArray(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            Console.Write($"{matrix[i, j]}");
-        }
-    }
-}
-
 void FillArray(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] = new Random().Next(1,10);
+            matrix[i,j] = new Random().Next(1, 10);
+            Console.Write($"{matrix[i,j]} ");
         }
-    } 
+        Console.WriteLine();
+    }
 }
 
-void OppositeArray(int[,] matrix)
+
+void OppositeArray(int[,] opposite)
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    for (int i = 0; i < opposite.GetLength(0); i++)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
+        for (int j = 0; j < opposite.GetLength(1); j++)
         {
-            if (matrix[i, j] % 2 == 0 && matrix[i, j] > 0)
+            if (opposite[i,j] % 2 == 0)
             {
-                matrix[i, j] = - matrix[i, j];
-                Console.Write($"{matrix[i, j]}");
+                //opposite[i, j] = -opposite[i,j];
+                Console.Write($"{-opposite[i,j]} ");
             }
+            else
+            {
+                opposite[i, j] = opposite[i,j];
+                Console.Write($"{opposite[i,j]} ");
+            }
+            
         }
+        Console.WriteLine();
     } 
 }
 
 void Demo()
 {
-    int[,] matrix = new int[3, 3];
+    int[,] matrix = new int[3,3];
     FillArray(matrix);
-    Console.WriteLine();
-    PrintArray(matrix);
     Console.WriteLine();
     OppositeArray(matrix);
 }
